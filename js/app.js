7f347c84a46f00b6b428c1e7067514d5e7aa7802
HDMejia21 <hdmejia@unitec.edu>
2025-12-18 15:47:37 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,44 @@
+// Dom7
+var $$ = Dom7;
+
+// Init App
+var app = new Framework7({
+  	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: true
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'Order',
+	panel: {
+		swipe: true,
+	},
+  	routes: [
+  {
+    path: '/index/',
+    url: 'index.html',
+    name: 'index'
+  },
+  {
+    path: '/categories/',
+    url: 'categories.html',
+    name: 'categories'
+  }
+],
+		
+	popup: {
+		closeOnEscape: true,
+		backdrop : false
+	},
+	sheet: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	actions: {
+		closeOnEscape: true,
+	}
+});
